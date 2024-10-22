2e1cf078d674142dc5b3d2257740013147f80727
SebaC12 <cordovaseba20@gmail.com>
2024-10-22 20:46:03 -0300

    Añadiendo Core

---
+++ src/test/java/com/sebacordova/DirectoresYPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebacordova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectoresYPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
